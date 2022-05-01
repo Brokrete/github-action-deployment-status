88c12700d3cabbd9fcb2aa4dc67f02872919a512
Oleksii Babichev <ababichev2404@gmail.com>
2022-05-01 22:14:46 +0200

    Main implementation

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,79 @@
-import * as core from '@actions/core'
-import {wait} from './wait'
+import * as core from "@actions/core";
+import * as github from "@actions/github";
 
-async function run(): Promise<void> {
+type DeploymentState =
+  | "error"
+  | "failure"
+  | "inactive"
+  | "in_progress"
+  | "queued"
+  | "pending"
+  | "success";
+
+async function run() {
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
+    const context = github.context;
 
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+    const pr = core.getInput("pr", { required: false }) === "true";
+    const prId = core.getInput("pr_id", {required: false}) || 0;
 
-    core.setOutput('time', new Date().toTimeString())
-  } catch (error) {
-    if (error instanceof Error) core.setFailed(error.message)
+    const logUrl = pr ?
+      `https://github.com/${context.repo.owner}/${context.repo.repo}/pull/${prId}/checks` :
+      `https://github.com/${context.repo.owner}/${context.repo.repo}/commit/${context.sha}/checks`;
+
+    const token = core.getInput("token", { required: true });
+    const ref = core.getInput("ref", { required: false }) || context.ref;
+    const url = core.getInput("target_url", { required: false }) || logUrl;
+    const payload = JSON.parse(`{"web_url": "${url}"}`)
+    const environment = core.getInput("environment", { required: false }) || "production";
+    const description = core.getInput("description", { required: false });
+
+    let deploymentId = parseFloat(core.getInput("deployment_id", { required: false }) || "0")
+
+    const status: DeploymentState = (core.getInput("status", { required: false }) as DeploymentState) || "pending";
+    const autoMerge = core.getInput("auto_merge", { required: false }) === "true";
+    const transientEnvironment = core.getInput("transient_environment", { required: false }) === "true"
+
+    const octokit = github.getOctokit(token)
+
+    if (!deploymentId || deploymentId === 0) {
+      const deployment = await octokit.rest.repos.createDeployment({
+        owner: context.repo.owner,
+        repo: context.repo.repo,
+        ref: ref,
+        payload,
+        required_contexts: [],
+        environment,
+        transient_environment: transientEnvironment,
+        auto_merge: autoMerge,
+        description
+      });
+
+      if(deployment.status !== 201){
+        throw new Error(`Failed to create deployment: ${deployment.status}`);
+      }
+
+      deploymentId = deployment.data.id;
+    }
+
+    const deploymentStatus = await octokit.rest.repos.createDeploymentStatus({
+      ...context.repo,
+      deployment_id: deploymentId,
+      state: status,
+      log_url: logUrl,
+      environment_url: url,
+      description
+    });
+
+    if(deploymentStatus.status !== 201){
+      throw new Error(`Failed to create deployment status: ${deploymentStatus.status}`);
+    }
+
+    core.setOutput("deployment_id", deploymentId);
+  } catch (error: any) {
+    core.error(error);
+    core.setFailed(error.message);
   }
 }
 
-run()
+run();
